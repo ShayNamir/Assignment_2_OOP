24f1871648187dc9c207fc5cfe35b5170b219575
shaynamir <shaynamir@gmail.com>
2024-02-07 16:53:55 +0200

    start building the User class

---
+++ SocialNetwork.py
@@ -0,0 +1,8 @@
+# SocialNetwork.py
+
+class SocialNetwork:
+    def __init__(self,name):
+        # Initialize the SocialNetwork class
+        self.name = name
+
+    #Methods
---
+++ User.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, password, users):
+        self.name = name
+        self.password = password
+        self.following = users
