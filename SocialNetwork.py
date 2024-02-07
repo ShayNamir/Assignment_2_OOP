fc1d76b2b9e28b44ea4198a027eaf269a603e4c9
shaynamir <shaynamir@gmail.com>
2024-02-07 17:50:25 +0200

    finish initialing SocialNetwork and User classes

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,8 +1,32 @@
 # SocialNetwork.py
+from User import User
+
 
 class SocialNetwork:
-    def __init__(self,name):
+    def __init__(self, name):
         # Initialize the SocialNetwork class
         self.name = name
+        self.users = set()  # Set that's contained all the user that's in the network
 
-    #Methods
+    # Methods
+    def __get_user_by_name__(self, name):
+        for user in self.users:
+            if name==user.get_name():
+                return user
+
+    '''
+    This function will get a name and a password and register the user to this network
+    '''
+    def sign_up(self, name, passw):
+        user = User(name, passw, True)  # The user will be logged in
+        self.users.add(user)  # Add the user to this network
+
+    def log_out(self,name):
+        user = self.__get_user_by_name__(name)
+        user.user_log_out()
+
+    def log_in(self, name, password):
+        user = self.__get_user_by_name__(name)  # Get the user
+
+        if user.check_pass(password):  # Check the password
+            user.user_log_in()  # If correct-> log in
--- User.py
+++ User.py
@@ -1,5 +1,36 @@
+"""
+Do we need to check for existing username before adding?
+"""
+
 class User:
-    def __init__(self, name, password, users):
+    def __init__(self, name, password, is_in):
         self.name = name
         self.password = password
-        self.following = users
+        self.followers = set()  # Initialize an empty set for followers
+        self.isLogIn = is_in
+
+    '''
+    This function will get a user and follow him
+    '''
+    def follow(self, user):
+        self.followers.add(user)
+
+    '''
+    This function will get a user and remove the follow from him, if exist 
+    '''
+    def unfollow(self, user):
+        self.followers.remove(user)
+
+    def user_log_in(self):
+        self.isLogIn = True
+
+    def user_log_out(self):
+        self.isLogIn = False
+
+    def get_name(self):
+        return self.name
+
+    def check_pass(self, password):
+        if self.password == password:
+            return True
+        return False
